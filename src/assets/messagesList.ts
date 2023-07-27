b98a3738f9381917c817ecf02c0ac6030cb7e72c
Notand105 <urzua.fernando.f@gmail.com>
2023-07-26 23:42:07 -0400

    new messages

--- src/assets/messagesList.ts
+++ src/assets/messagesList.ts
@@ -42,7 +42,98 @@ messagesData = [
      title:'Despedida',
      description : 'Chao te amo chao te amo cosita hermosa preciosa bonita bien hecha te amo chao te amo chao te amo divina linda cony preciosa la mejor del mundo me encantas lucesita verdecita chao te amo chao te amo...'   
     },
+    {
+     id:8,
+     title:'Haloween Boo',
+     description : 'No importa cuanto tiempo pase, siempre me voy a acordar de la vez que salimos disfrazados y a mi me reconocieron bastante y a ti re poquito jaja, la pasé muy bien contigo esa noche, disfruto mucho hacer cosas así contigo'   
+    },
+    {
+     id:9,
+     title:'Caballos',
+     description : '7 de febrero 2022, subimos el cerrito detras de tu casa en la playa y aunque aún no estabamos juntos quería pasar mucho tiempo pegado a tí, accedí a a tomarnos muchas fotos para poder estar bien cerquita, ah y ademas vimos unos cuantos caballos'   
+    },
+    {
+     id:10,
+     title:'Perfil',
+     description : 'Algo que jamás me va a dejar de encantar es tu carita de perfil cuando me miras hacia atrás, no se como explicarlo, te ves tan hermosa que el resto del mundo desaparece, tu carita es una obra de arte'   
+    },
+    {
+     id:11,
+     title:'Arena',
+     description : 'Lo mejor de la playa es que como solo yo llevo mi toalla grande, podemos estar acostaditos los dos bien juntitos'   
+    },
+    {
+     id:12,
+     title:'Flor',
+     description : 'Siempre que vamos caminando y encuentras una flor bonita la quedas mirando corazón, yo te miro a ti, porque tu carita cuando ves algo que te gusta y te pones contenta es lo más hermoso del mundo'   
+    },
+    {
+     id:13,
+     title:'Picnic playero',
+     description : 'Debo admitir que cuando estamos en la playa e insisto en bajar, no es porque me encante el mar o el sol, para mi lo más importante es estar solito contigo, y el caminar solitos al super, elegir algo rico y despues comerlo en la arenita frente al mar es el paraiso.'   
+    },
+    {
+     id:14,
+     title:'Amor',
+     description : 'Te amo, sin dudas y con todo mi corazón, contigo lo tengo todo y no quiero que acabe nunca'   
+    },
+    {
+     id:15,
+     title:'Fotografo',
+     description : 'No me gusta tomarme muchas fotos, sin embargo, lo que si me gusta mucho es sacarte fotos a ti, me gusta sacar mil y que puedas escoger tus favoritas y me gusta como sabes editar super bien los colores en el lightroom, tienes talento para eso, te quedan muy bonitas'   
+    },
+    {
+     id:16,
+     title:'Cine',
+     description : 'Como puedes inferir por el titulo, ir al cine contigo es una de mis actividades favoritas, y me pregunto si me gusta tanto porque me gusta ver peliculas contigo o porque me encanta el tiempo que pasamos antes comprando cositas y despues hablando de si nos gustó no, bueno de cualquier manera, no quiero que dejemos de ir nunca'   
+    },
+    {
+     id:17,
+     title:'Adicción',
+     description : 'Tus piernas, pecho, cintura y carita. Cada parte de tu cuerpo me tiene loquito, eres ideal para mi, me tienes loquito por ti'   
+    },
+    {
+     id:18,
+     title:'Masterchef',
+     description : 'Como chef de nivel mundial que soy, eres la única con el honor de probar mis preparaciones, jajaja, fuera de bromas me encanta cocinar para ti, pero me encanta más cuando cocinamos juntos, como nuestras ricas sopaipillas o el postre de galletas y tritón que hicimos un día '   
+    },   
+    {
+     id:19,
+     title:'Propuesta',
+     description : 'Quiero darte un besito (no importa cuando leas esto) '   
+    },   
+    {
+     id:20,
+     title:'Un gay volando',
+     description : 'Corazoncito, he de admitir que me has contagiado mucho de tu gusto musical, sin ti jamas hubiera tenido canciones del Harry o del Lui en mis me gustas, y ahora los escucho consistentemente, asi que sienteté orgullosa jaja '   
+    },
+    {
+     id:21,
+     title:'Bromita',
+     description : 'Fideos de los chinos, sushi, hamburguesas, completos, pancitos del antoja2, pizza, yo. Asi es corazón, la única intención de este mensaje era darte hambre, pequeña broma'   
+    },
+    {
+     id:22,
+     title:'Changos',
+     description : 'Perdí'   
+    },
+    {
+     id:23,
+     title:'Pelo',
+     description : 'Me encantá tu pelo, siempre consigues que se te vea bonito, aún cuando recien despiertas. Cortito, larguito, ondulado o con chasquilla siempre te ha quedadó bien y siempre te huele muy bien, me encanta tenerlo en mi pechito y perdón por tirartelo todo el tiempo'   
+    },
+    {
+     id:24,
+     title:'Madre canina',
+     description : 'Recuerda que tu eres la legitima madre del terry corazón, asi que hay que estar siempre juntitos con nuestra familia de perritos y futuros gatitos'   
+    },    
+    {
+     id:25,
+     title:'La mejor',
+     description : 'Es hermoso cuando decimos lo mismo al mismo tiempo, cuando nos movemos al unisono, que nos riamos por cosas absurdas y que tengamos nuestros propios jueguitos como el pooddle paso, me encanta tu sentido del humor y me gusta poder hacerte reir con el mio, me encanta nuestra relación y me encantas tú, eres simplemente la mejor'   
+    },
 ] 
+
 
 
 export {messagesData}
