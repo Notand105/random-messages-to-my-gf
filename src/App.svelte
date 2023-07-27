2af762cb36daa024a1381b92b3b0fd75c9498d1b
notand <urzua.fernando.f@gmail.com>
2023-07-26 22:38:11 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,155 @@
+<script lang="ts">
+  import { totalmessages } from "./stores/messagescounter";
+  import { messagesData } from "./assets/messagesList";
+
+  let randomnumber : number   
+  totalmessages.set(messagesData.length)
+
+  console.log(messagesData)
+  $: message = messagesData[randomnumber]
+
+ //get a random number but not the same as the last one 
+  const getRandom = () =>{
+    let max = $totalmessages
+    let random : number = randomnumber 
+    while(random == randomnumber ){
+      random = Math.floor(Math.random() * (max))
+    }
+    randomnumber = random
+
+  }
+  
+  getRandom()
+
+</script>
+<div class="container">
+  <header class="header">
+    <h1>Mis mensajes de amor para ti corazón</h1>
+    <h3>Actualmente {$totalmessages} mensajitos disponibles lucesita</h3>
+  </header>
+  <main class="messages">
+      <div class="next">
+        <button class="btn" on:click={getRandom}>Nuevo mensajito</button>
+      </div>
+      <div class="mess glass">
+        {#if message}
+        <h2>{message.title}</h2>
+        <h3>mensaje #{message.id}</h3>
+        <p>{message.description}</p> 
+        {/if}
+      </div>
+  </main>
+</div>
+
+<style>
+  .container{
+    height: 100vh;
+    min-width: 100vw;
+    display: grid;
+    grid-template-rows: auto 1fr ; 
+    gap: 1rem;
+    background:linear-gradient(to right, rgb(213, 111, 192), rgb(49, 128, 133)); 
+  
+  }
+  .header{
+    text-align: center;
+    padding: 3rem 8vw; 
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    gap: 5vh;
+    max-height: 50vh;
+  }
+  .messages{
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+    justify-content: center;
+    width: 90%;
+    margin: 0px auto;
+    gap: 2rem;
+  }
+  .mess{
+    width: 70%;
+    height: 80%;
+    padding: 0.5em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    border: 1px solid gray;
+    border-radius: 15px;
+  }
+  .btn{
+    padding: 1rem;
+    margin-bottom: 2rem; 
+    background-color: rgb(184, 97, 167);
+    border: none;
+    border-radius: 15px;
+    cursor: pointer;
+    transition: .2s ease;
+  }
+  .btn:hover{
+    transform: scale(1.01);
+    transition: .2s ease;
+  }
+  .glass{
+    background: linear-gradient(135deg, rgba(255,255,255,0.1), rgba(255,255,255,0));
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(50px);
+    border-radius: 1rem;
+    border: 1px solid  rgba(255,255,255,0.18);
+    box-shadow: 0 8px 32px 0  rgba(0,0,0,0.37);
+  }    
+  p{
+    max-width: 50ch;
+    text-align: justify;
+    word-spacing: 0.25rem;
+  }
+  h1,h2,h3,p{
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+  h1{
+    font-size: 4rem;
+  }
+  h2{
+    font-size: 2rem;
+    text-align: center;
+  }
+  h3{
+    font-size: 1.5rem;
+    text-align: center;
+  }
+  @media (max-width : 800px){
+    h1{
+      font-size: 2.5rem;
+    }
+    h2{
+      font-size: 1.6rem;
+    }
+    h3{
+      font-size: 1rem;
+    }
+    .mess{
+      width: 100%;
+    }
+    p{
+      font-size: 1.2rem;
+    }
+  }
+  @media (max-width : 600px){
+    h1{
+      font-size: 2rem;
+    }
+    p{
+      font-size: 1rem;
+    }
+  }
+  @media (min-width : 800px){
+    p{
+      font-size: 1.4rem;
+    }
+  }
+
+</style>
---
+++ src/assets/messagesList.ts
@@ -0,0 +1,48 @@
+type message = {
+    id : number,
+    title: string,
+    description : string
+}
+
+let messagesData : message[] = []
+
+messagesData = [
+    {
+     id:1,
+     title:'Mirando el piso',
+     description : 'Tu actividad favorita en nuestra temporada uno, probablemente miraste más el piso que mi cara esos primeros dias o hasta semanas cuando apenas nos conociamos, me encantaba ponerte nerviosa, y me alegro de poder seguir haciendolo cuando sonrio y me muerdo el labio <3'   
+    },
+    {
+     id:2,
+     title:'Galeria',
+     description : 'Estoy escribiendo esto en 26-07-2023, este día me dijiste por instagram que debería borrar muchas de nuestras fotos de la galeria, lo chistoso es que utilizaré esas mismas fotos para recordar e inspirarme para redactar todas estos mensajes muajaja'   
+    },
+    {
+     id:3,
+     title:'Elefante',
+     description : 'Elefante'   
+    },
+    {
+     id:4,
+     title:'Cony Simpson',
+     description : '¿Te acuerdas del video del homero con los barcos y tu pelo?, bueno cuando hice ese video no estabamos juntos, pero aun así me importabas tanto que me esforcé en editarlo por una cantidad considerable de tiempo para ver si te podia sacar una sonrisa '   
+    },
+    {
+     id:5,
+     title:'Perrito perdido',
+     description : 'Me acordé de la vez que encontramos al mino y a su compadre el guapo, recordandolo es un momento super lindo, unimos nuestras fuerzas para ayudar a dos animalitos solitos, a pesar de que no estabamos juntos en ese momento lo recuerdo con mucho cariño, porque me dejó ver lo muy linda que eres y lo bueno que es tu corazoncito '   
+    },
+    {
+     id:6,
+     title:'JIM',
+     description : 'No jim del nombre de persona, jim de como pronuncias HIMYM, me encanta como esa fue la serie unica que pude pegarte, si bien hemos visto muchas series y peliculas juntos me alegro que la que te gustó y siempre me pidas ver sea justo mi favorita, debe ser que estamos hechos el uno para el otro, como el Marshall y la Lily '   
+    },
+    {
+     id:7,
+     title:'Despedida',
+     description : 'Chao te amo chao te amo cosita hermosa preciosa bonita bien hecha te amo chao te amo chao te amo divina linda cony preciosa la mejor del mundo me encantas lucesita verdecita chao te amo chao te amo...'   
+    },
+] 
+
+
+export {messagesData}
